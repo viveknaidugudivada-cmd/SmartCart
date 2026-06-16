9d921aa4730ab6b045037a24390a82c750f77a63
Gudivada Vivek Naidu <viveknaidugudivada@gmail.com>
2026-06-16 11:04:22 +0530

    Clear sensitive data from config.py
    Removed sensitive information from configuration.

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ SECRET_KEY = "abc123"   # used for sessions
 # MySQL Database Configuration
 DB_HOST = "localhost"
 DB_USER = "root"
-DB_PASSWORD = "Vivek@10303"  # keep empty if no password
+DB_PASSWORD = ""  # keep empty if no password
 DB_NAME = "smartcart_db"
 
 
@@ -12,7 +12,7 @@ MAIL_SERVER = 'smtp.gmail.com'
 MAIL_PORT = 587
 MAIL_USE_TLS = True
 MAIL_USERNAME = 'viveknaidugudivada@gmail.com'
-MAIL_PASSWORD = 'qhpc ufvd pgmk tgda'   # Gmail App Password
+MAIL_PASSWORD = ''   # Gmail App Password
 
-RAZORPAY_KEY_ID = "rzp_test_SfPqHqlHt7XgRS"
-RAZORPAY_KEY_SECRET = "QofccviCPV8WZahPrrnBCiuN"
+RAZORPAY_KEY_ID = ""
+RAZORPAY_KEY_SECRET = ""
